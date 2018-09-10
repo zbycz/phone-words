508d2974f17282bea04162f62d930c63626e0e05
Pavel Zbytovský <zbytovsky@gmail.com>
2018-09-10 19:12:06 +0200

    config - added typescript

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-import "./App.js";
+import "./App";
 import "./index.css";
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
+const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+    entry: path.join(__dirname, "/src/index.ts"),
+    resolve: {
+        extensions: [".tsx", ".ts", ".js", ".jsx"],
+    },
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
