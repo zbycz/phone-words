470a63a4c2aecdd5ff6a3efa92ae43c9ae8ca126
Pavel Zbytovský <zbytovsky@gmail.com>
2018-09-11 16:38:46 +0200

    web - first prototype + added bootstrap css

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,22 @@ import * as ReactDOM from 'react-dom';
 const App = () => {
     console.log('render'); // tslint:disable-line:no-console
     return (
-        <div>
-            <p>React here!</p>
+        <div className="container pt-3">
+            <div className="card-deck mb-3 text-center">
+                <div className="card mb-4 box-shadow">
+                    <div className="card-header">
+                        <h4 className="my-0 font-weight-normal">Phone words generator</h4>
+                    </div>
+                    <div className="card-body">
+                        <div className="input-group mb-3">
+                            <input type="text" className="form-control" placeholder="Enter number..." aria-label="Enter number..." aria-describedby="button-addon2" />
+                            <div className="input-group-append">
+                                <button className="btn btn-outline-primary" type="button" id="button-addon2">Generate words</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+import 'bootstrap';
+
 import './App';
 import './styles/index.scss';
